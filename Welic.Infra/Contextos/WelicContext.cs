714f542f357bb7edbded43ffcd12c56923164f26
Lucas Ranuzi Rocha <lucas.uzi@hotmail.com>
2018-08-20 23:31:08 -0300

    Estruturando projeto Welic

---
+++ Welic.Infra/Contextos/WelicContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Welic.Api.Contexts
+{
+    public class WelicContext : DbContext
+    {
+        public WelicContext()
+            :base("WelicConectionString")
+        {
+
+        }       
+        //public DbSet<Autor> Autores { get; set; }      
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Configurations.Add(new AutorMap());
+            
+
+        }
+    }
+}
