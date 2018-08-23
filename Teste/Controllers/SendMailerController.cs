3f62b7a83c22eacb0c767945a7796257eac64905
Lucas Ranuzi Rocha <lucas.uzi@hotmail.com>
2018-08-22 23:46:16 -0300

    atualização da HomePage

---
+++ Teste/Controllers/SendMailerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Teste.Models;
+
+namespace Teste.Controllers
+{
+    public class SendMailerController : Controller
+    {
+        // GET: SendMailer
+        public ActionResult Index()
+        {
+            return View();
+        }
+  
+        [HttpPost]
+        public ViewResult Index(MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                //Instância classe email
+                MailMessage mail = new MailMessage();
+                mail.To.Add("contato@welic.app");
+                mail.From = new MailAddress("contato@welic.app");
+                mail.Subject = $"Olá Welic! Quero seguir suas notícias";
+                string Body = $"Meu Nome é {_objModelMail.Nome} {_objModelMail.LastName} e meu e-mail: {_objModelMail.Email}. Gostaria de Receber suas noticias. Aguardo seu contato! Obrigado";
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+
+                //Instância smtp do servidor, neste caso o gmail.
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    UseDefaultCredentials = false,
+                    Credentials = new System.Net.NetworkCredential
+                    ("contato@welic.app", "@CW22082018elic"),// Login e senha do e-mail.
+                    EnableSsl = true
+                };
+                smtp.Send(mail);
+                return View("Index", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Teste/Models/MailModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Teste.Models
+{
+    public class MailModel
+    {
+        [Required(ErrorMessage ="Obrigatório o Primeiro Nome")]
+        [Display(Name ="Digite seu primeiro nome aqui!")]        
+        public string Nome { get; set; }
+        
+        [Display(Name = "Digite seu ultimo nome aqui!")]
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório o e-mail")]
+        [Display(Name = "Digite seu e=mail aqui!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
