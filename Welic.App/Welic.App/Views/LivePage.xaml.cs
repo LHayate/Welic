908e4468cda65c6c40d311d4ccfae8e247493439
Lucas Ranuzi Rocha <lucas.uzi@hotmail.com>
2018-10-18 15:36:39 -0300

    Alteando Framework do Projeto para atender ao servidor.

--- Welic.App/Welic.App/Views/LivePage.xaml.cs
+++ Welic.App/Welic.App/Views/LivePage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
 using Welic.App.Services.VideoPlayer;
 using Welic.App.ViewModels;
 using Welic.App.ViewModels.Base;
@@ -10,49 +12,99 @@ namespace Welic.App.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LivePage : ContentPage
 	{
-		public LivePage ()
+	    private IPlaybackController PlaybackController => CrossMediaManager.Current.PlaybackController;
+        public LivePage ()
 		{
 			InitializeComponent ();
 		    BindingContext = ViewModelLocator.Resolve<LiveViewModel>();
-		}
+		    this.volumeLabel.Text = "Volume (0-" + CrossMediaManager.Current.VolumeManager.MaxVolume + ")";
+		    //Initialize Volume settings to match interface
+		    int.TryParse(this.volumeEntry.Text, out var vol);
+		    CrossMediaManager.Current.VolumeManager.CurrentVolume = vol;
+		    //CrossMediaManager.Current.VolumeManager.Muted = false;
+
+		    CrossMediaManager.Current.PlayingChanged += (sender, e) =>
+		    {
+		        Device.BeginInvokeOnMainThread(() =>
+		        {
+		            ProgressBar.Progress = e.Progress;
+		            Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+		        });
+		    };
+        }
 
 	    public LivePage(params object[] obj)
 	    {
 	        InitializeComponent();
 	        BindingContext = new LiveViewModel(obj);
-        }
-	    
+	        this.volumeLabel.Text = "Volume (0-" + CrossMediaManager.Current.VolumeManager.MaxVolume + ")";
+	        //Initialize Volume settings to match interface
+	        int.TryParse(this.volumeEntry.Text, out var vol);
+	        CrossMediaManager.Current.VolumeManager.CurrentVolume = vol;
+	        //CrossMediaManager.Current.VolumeManager.Muted = false;
 
-	    private void Button_OnClicked(object sender, EventArgs e)
-	    {	        
-	        if (videoPlayer.Status == VideoStatus.Playing)
+	        CrossMediaManager.Current.PlayingChanged += (sender, e) =>
 	        {
-	            videoPlayer.Pause();
-	        }
-	        else if (videoPlayer.Status == VideoStatus.Paused)
-	        {
-	            videoPlayer.Play();
-	        }
-	        else if (videoPlayer.Status == VideoStatus.NotReady)
-	        {
-	            videoPlayer.Play();
-	        }	       
+	            Device.BeginInvokeOnMainThread(() =>
+	            {
+	                ProgressBar.Progress = e.Progress;
+	                Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+	            });
+	        };
         }
-	    void OnPlayPauseButtonClicked(object sender, EventArgs args)
+
+	    protected override void OnAppearing()
 	    {
-	        if (videoPlayer.Status == VideoStatus.Playing)
-	        {
-	            videoPlayer.Pause();
-	        }
-	        else if (videoPlayer.Status == VideoStatus.Paused)
-	        {
-	            videoPlayer.Play();
-	        }
+	        videoView.Source = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
 	    }
 
-	    void OnStopButtonClicked(object sender, EventArgs args)
+	    void PlayClicked(object sender, System.EventArgs e)
 	    {
-	        videoPlayer.Stop();
+	        PlaybackController.Play();
 	    }
+
+	    void PauseClicked(object sender, System.EventArgs e)
+	    {
+	        PlaybackController.Pause();
+	    }
+
+	    void StopClicked(object sender, System.EventArgs e)
+	    {
+	        PlaybackController.Stop();
+	    }
+	    private void SetVolumeBtn_OnClicked(object sender, EventArgs e)
+	    {
+	        int.TryParse(this.volumeEntry.Text, out var vol);
+	        CrossMediaManager.Current.VolumeManager.CurrentVolume = vol;
+	    }
+
+	    private void MutedBtn_OnClicked(object sender, EventArgs e)
+	    {
+	        //if (CrossMediaManager.Current.VolumeManager.Muted)
+	        //{
+	        //    CrossMediaManager.Current.VolumeManager.Muted = false;
+	        //    mutedBtn.Text = "Mute";
+	        //}
+	        //else
+	        //{
+	        //    CrossMediaManager.Current.VolumeManager.Muted = true;
+	        //    mutedBtn.Text = "Unmute";
+	        //}
+	    }
+     //   private void Button_OnClicked(object sender, EventArgs e)
+	    //{	        
+	    //    if (videoPlayer.Status == VideoStatus.Playing)
+	    //    {
+	    //        videoPlayer.Pause();
+	    //    }
+	    //    else if (videoPlayer.Status == VideoStatus.Paused)
+	    //    {
+	    //        videoPlayer.Play();
+	    //    }
+	    //    else if (videoPlayer.Status == VideoStatus.NotReady)
+	    //    {
+	    //        videoPlayer.Play();
+	    //    }	       
+     //   }	    
     }
 }
